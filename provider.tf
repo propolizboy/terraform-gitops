4bdf7a7965d961c5d965e416d7e092a8e998660e
propolizboy <propolizboy@gmail.com>
2022-12-13 15:22:31 +0700

    init infra

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+    required_providers{
+        aws = {
+            source = "hashicorp/aws"
+            version = "4.29.0"
+        }
+    }
+    cloud {
+        organization = "Ruj-DevOps-Training"
+        workspaces {
+          name ="devops-infra"
+        }
+    }
+}
+provider "aws"{
+    region = "ap-southeast-1"
+}
